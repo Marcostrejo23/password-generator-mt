9e24a91c5cec7781c9ee3121985aebada4c74092
Marcos Trejo <marcostrejo23@gmail.com>
2021-07-26 20:39:50 -0700

    final push for completed generator

--- script.js
+++ script.js
@@ -7,18 +7,6 @@ var characterCount = ["1","2","3","4","5","6","7","8","9","10","11","12","13","1
 
 var generateBtn = document.querySelector("#generate");
 
-
-// function chooseChars(){
-//   window.alert ('chooseChars')
-//     var characters = window.prompt ("how many characters");
-//     var lowerCase = window.confirm ("do you want lowerCase?");
-// }
-// function writePassword(){
-//     window.alert('clicked')
-//     var password = chooseChars();
-//     console.log(password)
-// }
-
   generateBtn.addEventListener("click", writePassword);
     
     function writePassword() {
@@ -28,7 +16,7 @@ var generateBtn = document.querySelector("#generate");
         return false 
       }
       do{ 
-        var passLength = prompt("how many chracters? 8-16.")
+        var passLength = parseInt(prompt("how many chracters? 8-16."))
         if (passLength < 8){
           alert("password must be longer");
         }else if (passLength > 16){
@@ -40,43 +28,24 @@ var generateBtn = document.querySelector("#generate");
       var include = '';
       var total = '';
 
-      // var passInt = parseInt(passLength);
+
 
       if (!passLength && !specialCharacters && !capAlpha){
-        result += "does not meet the criteria to generate a password";
-        return include
-      } else
-        if (specialCharacters) {
-         total = total + specialCharacters;
-      } if (capAlpha) {
-        total = total +capAlpha; 
-      }
-      for (let i = 0; i < passLength.length; i++) {
-        include += total.charAt(Math.floor(Math.random() * total.length)); 
-      } 
-      return total;
-
-
- 
-
+        alert("does not meet the criteria to generate a password");
   
+      } else{
+        if (specialCharacters) {
+         total = total + special.join('');
+      } if (capAlpha) {
+        total = total + capAlphabet.join(''); 
+      }
+      console.log(total)
+      for (let i = 0; i < passLength; i++) {
+        include += total.charAt(Math.floor(Math.random() * (total.length-1))); 
+      } 
+      console.log(include)
+       var passwordText = document.querySelector("#password");
+            passwordText.value = include;
 
-
-      // var passwordText = document.querySelector("#password");
-      
-      // function randomGen(alphabet , special , capAlphabet , number){
-      // var randIndex = Math.floor(Math.random() * Array.length);
-      // var randElement = Array[randIndex];
-    
-      // return randElement;
-    
-      // }
-      // passwordText.value = password;
-    
-      function createPassword() {
-        var password = writePassword();
-        var passwordText = document.querySelector("#password");
-        passwordText.value = password;
-      };
-      generateBtn.addEventListener("click", createPassword);
+    };
     };
